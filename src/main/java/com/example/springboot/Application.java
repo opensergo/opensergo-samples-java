1b1ebf67f50f27df78572c692b13f88e051e9c4a
luyanbo <robert.lyb@alibaba-inc.com>
2022-03-30 18:10:08 +0800

    named example-spring-cloud

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,12 +1,7 @@
 package com.example.springboot;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -14,6 +9,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 
 }
